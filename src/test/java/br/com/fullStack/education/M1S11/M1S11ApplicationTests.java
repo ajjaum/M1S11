71a34c36d3cff704905b15ad108ec1a04073afe1
ajjaum <j061701@dac.unicamp.br>
2024-04-10 15:31:49 -0300

    Iniciando Atividade

---
+++ src/test/java/br/com/fullStack/education/M1S11/M1S11ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullStack.education.M1S11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
